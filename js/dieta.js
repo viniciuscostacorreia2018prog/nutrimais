d08399781f2e0b4f9c4e8f967d072eac05c64f4f
nutrimuitomais <viniciuscostacorreia2018@gmail.com>
2026-01-04 20:29:12 -0300

    Update dieta.js

--- js/dieta.js
+++ js/dieta.js
@@ -10,8 +10,30 @@ function selecionarAlimento(refeicao, alimento) {
   if (!dieta[refeicao].includes(alimento)) {
     dieta[refeicao].push(alimento);
   }
+  atualizarBotao();
 }
 function finalizarDieta() {
   localStorage.setItem('dietaUsuario', JSON.stringify(dieta));
   window.location.href = 'portal.html';
+}
+function dietaValida() {
+  return (
+    dieta.cafeManha.length > 0 &&
+    dieta.almoco.length > 0 &&
+    dieta.cafeTarde.length > 0 &&
+    dieta.janta.length > 0
+  );
+}
+
+function finalizarDieta() {
+  if (!dietaValida()) {
+    alert('Por favor, selecione pelo menos um alimento em cada refeição.');
+    return;
+  }
+
+  localStorage.setItem('dietaUsuario', JSON.stringify(dieta));
+  window.location.href = 'portal.html';
+}
+function atualizarBotao() {
+  document.getElementById('btnSeguinte').disabled = !dietaValida();
 }
