7c2d9058551544fb519422d55758425500eebd1e
nutrimuitomais <viniciuscostacorreia2018@gmail.com>
2026-01-13 20:06:05 -0300

    Update carregando.js

--- js/carregando.js
+++ js/carregando.js
@@ -1,16 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   const btnContinuar = document.getElementById('p4Continuar');
-
   if (!btnContinuar) return;
 
   btnContinuar.addEventListener('click', function () {
 
-    // evita clique duplo
     btnContinuar.disabled = true;
     btnContinuar.classList.add('desativado');
 
-    // redireciona para pagamento
     window.location.href = 'pagamento.html';
 
   });
